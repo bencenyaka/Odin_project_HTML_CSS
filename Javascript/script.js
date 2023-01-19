a6fda3b2e2500a107d37d5375d71f92720c58ea9
bencenyaka <nyakabence2003@gmail.com>
2023-01-19 12:44:31 +0100

    JS

---
+++ Javascript/script.js
@@ -0,0 +1,32 @@
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+
+console.log(a);
+
+let b = 7 * a;
+
+console.log(b);
+
+const max = 57;
+
+const actual = max - 13;
+
+const percentage = actual / max;
+
+let string = "";
+
+if (actual < max){
+    string = "Kisebb";
+    console.log(string);
+}
+else{
+    string = "Nagyobb";
+    console.log(string);
+}
+
+
+let admin, name;
+name = "John";
+admin = name;
+alert(admin);
